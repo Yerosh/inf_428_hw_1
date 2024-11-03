031aab22435d18cd7c6d19e11813974c09c2789e
Yerosh <91179077+Yerosh@users.noreply.github.com>
2024-11-03 21:38:04 +0500

    Update lcode_349_solution.py

--- task_1/sets and dictionaries/lcode_349_solution.py
+++ task_1/sets and dictionaries/lcode_349_solution.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        newArr = set()
+        for num in nums1:
+            if num in nums2:
+                newArr.add(num)
+        return list(newArr)
